0d75d9ebdda3429d49bee9fa570520f1f4c19d07
TheDude1 <kohlerseverin@aol.de>
2013-11-27 16:21:04 +0100

    Update Breitensuche.java

--- uebung03/uebung03/Breitensuche.java
+++ uebung03/uebung03/Breitensuche.java
@@ -1,7 +1,9 @@
 package uebung03;
 /**
- * 
- * @author chris
+ * Generische Klasse Breitensuche die von Pathfinder erbt und das Interface  SearchStrategy implementiert
+ * die Klasse durchsucht Erbstufe auf Erbstufe vom bestimmten Knoten an alle Kindknoten nach einem Wert ist dieser im Knoten
+ * enthalten speichert er den Knoten in eine Liste die dann nachdem alle Knoten durchlaufen sidn ausgegeben wird.
+ * @author Chris Harsch und Severin Kohler
  *
  * @param <T>
  */
@@ -9,7 +11,11 @@ public class Breitensuche<T> extends PathFinder implements SearchStrategy<T> {
 
 	@Override
 	/**
-	 * 
+	 * Methode durchsucht die Knoten ab dem Startknoten an nachdem Wert hierbei wird immer nur 1 Knoten
+	 * überprüft folgende werden in eine warteliste eingetragen und beim nächsten Durchlauf der Schleife 
+	 * überprüft. Abbruchbedingung ist hierbei das es keine Knoten mehr in der Warteschlange mehr gibt
+	 *@param Node<Node<T>> node ist der Übergebene Knoten(Startknoten) 
+	 * @param T toSearch ist der zu suchende Wert, Typ generisch
 	 */
 	public NodeListImpl<Node<T>> search(Node<Node<T>> node, T toSearch) {
 		NodeListImpl found = new NodeListImpl();
@@ -35,7 +41,9 @@ public class Breitensuche<T> extends PathFinder implements SearchStrategy<T> {
 
 	@Override
 	/**
-	 * 
+	 * Gibt den in search gespeicherten Wert zurück, dieser beinhalted den Suchweg
+	 * der Methode search beim letzten Aufruf.
+	 * @param path wird geerbt aus der Funktion Pathfinder
 	 */
 	public NodeListImpl<Node> getPath() {
 		return path;
