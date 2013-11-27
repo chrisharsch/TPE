a814e9194ffa4a6def1ddad2c39d36efd8d4b0a5
crunkchilla <chris.harsch91@googlemail.com>
2013-11-27 17:13:20 +0100

    Version 1.5

--- uebung03/search/Tiefensuche.java
+++ uebung03/search/Tiefensuche.java
@@ -7,8 +7,8 @@ import lists.NodeListImpl;
 
 /**
  * 
- * @author chris
- *
+ * @author Chris Harsch und Severin Kohler
+ * 
  * @param <T>
  */
 public class Tiefensuche<T> extends PathFinder implements SearchStrategy<T> {
@@ -16,12 +16,13 @@ public class Tiefensuche<T> extends PathFinder implements SearchStrategy<T> {
 	 * 
 	 */
 	private NodeListImpl<Node<T>> nodelist = new NodeListImpl<Node<T>>();
-/**
- * 
- * @param node
- * @param toSearch
- * @return
- */
+
+	/**
+	 * 
+	 * @param node
+	 * @param toSearch
+	 * @return
+	 */
 	private NodeListImpl<Node<T>> toSearch(Node<Node<T>> node, T toSearch) {
 		path.add(node);
 		if (toSearch.equals(node.getValue())) {
@@ -37,7 +38,10 @@ public class Tiefensuche<T> extends PathFinder implements SearchStrategy<T> {
 
 	@Override
 	/**
-	 * @return 
+	 * @param node Knoten von dem aus gesucht werden soll
+	 * @param toSearch der zu Suchende Wert mit dem alle Kinder von node und node selbst verglichen werden
+	 * @return gibt die Liste der Knoten mit dem uebereinstimmenden Values toSearch wieder
+	 * 
 	 */
 	public NodeListImpl<Node<T>> search(Node<Node<T>> node, T toSearch) {
 		path.clear();
@@ -46,7 +50,9 @@ public class Tiefensuche<T> extends PathFinder implements SearchStrategy<T> {
 
 	@Override
 	/**
+	 * Methode um den Pfad der letzten Suche wieder zu geben
 	 * 
+	 * @return Gibt eine Liste des letzten Suchpfades zurück
 	 */
 	public NodeListImpl<Node> getPath() {
 		return path;
